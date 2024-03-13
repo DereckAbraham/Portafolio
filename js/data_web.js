2b628a0fcfbc145dd075caf63ee5d3a83e4a6f2b
DereckAbraham <dacarranza@scl.edu.gt>
2024-03-13 13:01:10 -0600

    PERFIL CON DISENNO - Dacarranza

---
+++ js/data_web.js
@@ -0,0 +1,26 @@
+let webs = [
+    {
+        nombre: "Juego de Memoria",
+        img :"https://foundations.projectpythia.org/_images/GitHub-logo.png",
+        repositorio: "https://github.com/DereckAbraham/Memoria2024",
+        githubpage: "https://dereckabraham.github.io/Memoria2024/",
+        descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
+    },
+    {
+        nombre: "Maquetacion",
+        img :"https://foundations.projectpythia.org/_images/GitHub-logo.png",
+        repositorio: "https://github.com/DereckAbraham/Maquetacion?tab=readme-ov-file",
+        githubpage: "https://dereckabraham.github.io/Maquetacion/",
+        descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
+    },
+    {
+        nombre: "Habilidades",
+        img :"https://foundations.projectpythia.org/_images/GitHub-logo.png",
+        repositorio: "https://github.com/DereckAbraham/Profile?tab=readme-ov-file",
+        githubpage: "https://dereckabraham.github.io/Profile/",
+        descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
+    }
+
+];
+
+export {webs}
